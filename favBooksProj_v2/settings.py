ea5ac968be5aaabaad6157eb3c689800e944cf2f
Joseph Niosi <niosij@gmail.com>
2021-10-01 21:18:36 -0400

    added gitignore and s-key fix

--- favBooksProj_v2/settings.py
+++ favBooksProj_v2/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,14 +23,14 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '9i_ydv=atp_n@dio0gwjj@1x5r$cs#!=3*8g-p+hicigij8n1h'
+SECRET_KEY = str(os.getenv("SECRET_KEY"))
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(os.getenv("DEBUG_STATUS"))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
-
+print(str(os.getenv("SUPER_SECRET")))
 # Application definition
 
 INSTALLED_APPS = [
